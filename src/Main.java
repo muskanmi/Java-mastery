597c9cd67c21a56c4a84c2a59db1ae3665f06b5a
Muskan Mittal <muskanmittal@Muskans-MacBook-Pro.local>
2025-08-26 20:22:28 +0530

    java variables

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,11 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+        int age = 26;
+        int aGe = 30;
+
+        int marks = 98;
+        int secondMarks = 87;
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
