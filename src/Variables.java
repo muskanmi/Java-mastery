63db1aaab730656ed6ddc858c6d9facf2023a193
Muskan Mittal <muskanmittal@Muskans-MacBook-Pro.local>
2025-08-26 20:39:46 +0530

    data types in java

---
+++ src/Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+    public static void main(String[] args) {
+        boolean isPassed = true;
+        int count = 12;
+        byte marks = 13;
+        short studentsCount = 1235;
+        float pi = 3.14f;
+
+        double secondPi = 3.14;
+
+        System.out.print(count);
+        System.out.print(marks);
+        System.out.print(isPassed);
+        System.out.println(pi);
+        System.out.println(secondPi);
+    }
+}
