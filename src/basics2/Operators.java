057da7a74ac6a18503e6ccb0f49d96c147a76ad9
Muskan Mittal <mittalmuskan758@gmail.com>
2025-08-27 19:02:00 +0530

    learning operators in java

---
+++ src/basics2/Operators.java
@@ -0,0 +1,11 @@
+package basics2;
+
+public class Operators {
+    public static void main(String[] args) {
+//        operators
+        int a = 12;
+        int b= 7;
+        int c = a+b;
+        System.out.println(c);
+    }
+}
