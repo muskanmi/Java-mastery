b4fb1b20a9e23acb257fa8c4cd8effebfd50ee65
Muskan Mittal <mittalmuskan758@gmail.com>
2025-08-28 19:06:22 +0530

    learnt user input and conditional statements in java

--- src/basics2/IfElse.java
+++ src/basics2/IfElse.java
@@ -3,6 +3,7 @@ package basics2;
 public class IfElse {
     public static void main(String[] args) {
         int age = 4;
-        System.out.println("You can vote");
+        if(age >= 18)
+            System.out.println("You can vote");
     }
 }
