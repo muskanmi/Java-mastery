e368b9e3801a1aa85d24dea015913adc8d23b128
Muskan Mittal <mittalmuskan758@gmail.com>
2025-08-29 11:33:35 +0530

    code patterns in java

--- src/basics2/BreakAndContinue.java
+++ src/basics2/BreakAndContinue.java
@@ -27,5 +27,12 @@ public class BreakAndContinue {
             }
             System.out.println("printed "+count);
         }
+
+        for(int i=0; i<5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
