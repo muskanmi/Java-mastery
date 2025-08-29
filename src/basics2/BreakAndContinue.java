0edace4b2e148a17727ef647ecf31d914f6ea409
Muskan Mittal <mittalmuskan758@gmail.com>
2025-08-29 11:26:29 +0530

    learned continue and nested loops in java

--- src/basics2/BreakAndContinue.java
+++ src/basics2/BreakAndContinue.java
@@ -2,9 +2,30 @@ package basics2;
 
 public class BreakAndContinue {
     public static void main(String[] args) {
-        for (int i = 0; i <= 20; i+=2) {
-            System.out.println(i);
-            if(i>=10) break;
+//        for (int i = 0; i <= 20; i+=2) {
+//            System.out.println(i);
+//            if(i>=10) break;
+//        }
+
+//        int i =0;
+//        while(i<=5){
+//            System.out.println(i);
+//            i++;
+//            if(i==3) break;
+//        }
+
+
+//        for (int i = 0; i <= 20; i+=2) {
+//            if(i==2 || i==8) continue;
+//            if(i>=15) break; // or if(i==2 || i==8 || i>=15) continue;
+//            System.out.println("Gave toffee to "+i);
+//        }
+
+        for (int count = 0; count < 10; count++) {
+            for(int i=1; i<6; i++) {
+                System.out.print(i+" ");
+            }
+            System.out.println("printed "+count);
         }
     }
 }
