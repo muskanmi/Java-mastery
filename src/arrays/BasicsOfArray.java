cf93f142ce4580158646370f97d663248b82be44
Muskan Mittal <mittalmuskan758@gmail.com>
2025-08-30 19:18:02 +0530

    learning loops

---
+++ src/arrays/BasicsOfArray.java
@@ -0,0 +1,16 @@
+package arrays;
+
+public class BasicsOfArray {
+    public static void main(String[] args) {
+        int age[]; // declaration
+        age = new int[5]; // initialization
+
+//        another way is -
+        int age2[] = new int[5];
+
+        age[0] = 5;
+        age[1] = 2;
+
+        System.out.println(age[0]);
+    }
+}
