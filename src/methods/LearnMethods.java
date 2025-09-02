e4431c4345310cfb7b4835d29eb8ab0f4a9898a0
Muskan Mittal <mittalmuskan758@gmail.com>
2025-09-02 19:11:29 +0530

    learning methods in java

---
+++ src/methods/LearnMethods.java
@@ -0,0 +1,11 @@
+package methods;
+
+public class LearnMethods {
+    public static void main(String[] args) {
+        greet();
+    }
+
+    public static void greet() {
+        System.out.println("Hello Method");
+    }
+}
