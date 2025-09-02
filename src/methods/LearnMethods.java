b9d717cb8e07fa840751aa09d4ba5dc5692bbd23
Muskan Mittal <mittalmuskan758@gmail.com>
2025-09-02 19:21:29 +0530

    make the method of average

--- src/methods/LearnMethods.java
+++ src/methods/LearnMethods.java
@@ -3,9 +3,15 @@ package methods;
 public class LearnMethods {
     public static void main(String[] args) {
         greet();
+        average(3, 4);
     }
 
     public static void greet() {
         System.out.println("Hello Method");
+    }
+
+    public static void average(int a,  int b) {
+        int avg = (a+b)/2;
+        System.out.println("The average is: "+avg);
     }
 }
