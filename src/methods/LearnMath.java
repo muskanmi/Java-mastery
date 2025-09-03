bd294f10e5a9324b5889c215774e77599674708c
Muskan Mittal <mittalmuskan758@gmail.com>
2025-09-03 10:58:09 +0530

    learned math class methods

---
+++ src/methods/LearnMath.java
@@ -0,0 +1,10 @@
+package methods;
+
+public class LearnMath {
+    public static void main(String[] args) {
+        int a =4;
+        int b=7;
+        System.out.println(Math.min(a, b));
+        System.out.println(Math.max(a, b));
+    }
+}
