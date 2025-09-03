3dbd56357acb4cad41761496dfb88612f4a126fa
Muskan Mittal <mittalmuskan758@gmail.com>
2025-09-03 11:04:55 +0530

    learned random number between two given number with Math class

--- src/methods/LearnMath.java
+++ src/methods/LearnMath.java
@@ -6,5 +6,11 @@ public class LearnMath {
         int b=7;
         System.out.println(Math.min(a, b));
         System.out.println(Math.max(a, b));
+        System.out.println(getRandom(10, 20));
+    }
+
+    public static int getRandom(int a, int b) {
+//        return (int) (Math.random() * 6 )+ 1;
+        return (int) (Math.random()*(b-a+1) + a);
     }
 }
