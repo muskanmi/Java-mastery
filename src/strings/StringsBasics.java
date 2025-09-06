f01a4f54eef15c93993178eaa01e81d5e4153241
Muskan Mittal <mittalmuskan758@gmail.com>
2025-09-06 19:05:00 +0530

    learning string concept

--- src/strings/StringsBasics.java
+++ src/strings/StringsBasics.java
@@ -2,7 +2,8 @@ package strings;
 
 public class StringsBasics {
     public static void main(String[] args) {
-        String name = "Muskan";
+        String name = "Muskan"; // first way using string literal
+        String name2 = new String("Anuj"); // second way by using new keyword
         System.out.println(name);
     }
 }
