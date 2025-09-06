12013ef59f6daa8705da413e88df1123636b6cf5
Muskan Mittal <mittalmuskan758@gmail.com>
2025-09-06 19:09:37 +0530

    learning string concept

--- src/strings/StringsBasics.java
+++ src/strings/StringsBasics.java
@@ -4,6 +4,9 @@ public class StringsBasics {
     public static void main(String[] args) {
         String name = "Muskan"; // first way using string literal
         String name2 = new String("Anuj"); // second way by using new keyword
+        String samename = "Muskan"; // both name and samename is pointing to same reference of muskan. No new memory is create 2 times.
         System.out.println(name);
+
+        System.out.println(name == samename); // always give true because of same pointing reference
     }
 }
