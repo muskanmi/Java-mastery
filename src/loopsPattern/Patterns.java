54f2ca8a0095fac1fc74b3161333937e2e606596
Muskan Mittal <mittalmuskan758@gmail.com>
2025-09-09 19:11:46 +0530

    solving pattern question

--- src/loopsPattern/Patterns.java
+++ src/loopsPattern/Patterns.java
@@ -2,10 +2,30 @@ package loopsPattern;
 
 public class Patterns {
     public static void main(String[] args) {
-        int n = 7;
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
-                System.out.print("*");
+        // pattern 1
+
+//        int n = 7;
+//        for(int i=1; i<=n; i++){
+//            for(int j=1; j<=n; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+        //pattern 2
+//        int n = 5;
+//        for(int i=1; i<=n; i++){
+//            for(int j=1; j<=i; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+        // pattern 3
+        int n = 6;
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=i; j++){
+                System.out.print(j + " ");
             }
             System.out.println();
         }
