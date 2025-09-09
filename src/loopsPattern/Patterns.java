31b156f2755e77faf3c0e91184a84ccbc55b3829
Muskan Mittal <mittalmuskan758@gmail.com>
2025-09-09 19:22:41 +0530

    solving pattern question

--- src/loopsPattern/Patterns.java
+++ src/loopsPattern/Patterns.java
@@ -22,10 +22,22 @@ public class Patterns {
 //        }
 
         // pattern 3
-        int n = 6;
-        for(int i=1; i<=n; i++) {
-            for(int j=1; j<=i; j++){
-                System.out.print(j + " ");
+//        int n = 6;
+//        for(int i=1; i<=n; i++) {
+//            for(int j=1; j<=i; j++){
+//                System.out.print(j + " ");
+//            }
+//            System.out.println();
+//        }
+
+        // pattern 4
+        int n =6;
+        for(int i =1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
