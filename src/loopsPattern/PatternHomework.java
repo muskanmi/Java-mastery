9256e5adb2c82619aee47fadb291f3bd3fb1dd87
Muskan Mittal <mittalmuskan758@gmail.com>
2025-09-10 12:08:37 +0530

    solving pattern question

--- src/loopsPattern/PatternHomework.java
+++ src/loopsPattern/PatternHomework.java
@@ -91,18 +91,38 @@ public class PatternHomework {
 //        }
 
         // patytern 7
-        int n=6;
-        for(int i=1; i<=n; i++){
-            for(int j=i; j<n; j++){
+//        int n=6;
+//        for(int i=1; i<=n; i++){
+//            for(int j=i; j<n; j++){
+//                System.out.print(" ");
+//            }
+//            for(int j=1; j<=2*i-1; j++){
+//                if(j==1 || i==n || j == 2*i-1) {
+//                    System.out.print("*");
+//                } else{
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.println();
+//        }
+
+        // pattern 8
+        int n = 6;
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=i; j++){
                 System.out.print(" ");
             }
-//            System.out.print("*");
-            for(int j=1; j<=2*i-1; j++){
-                if(j==1 || i==n || j == 2*i-1) {
-                    System.out.print("*");
-                } else{
-                    System.out.print(" ");
-                }
+            for(int j=i; j<=n; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        for(int i=n-1; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print(" ");
+            }
+            for(int j=i; j<=n; j++){
+                System.out.print(j + " ");
             }
             System.out.println();
         }
