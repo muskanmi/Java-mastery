7f6e77449a617fea202f9fa635861d138519084d
Muskan Mittal <mittalmuskan758@gmail.com>
2025-09-10 11:22:33 +0530

    solving pattern question

--- src/loopsPattern/PatternHomework.java
+++ src/loopsPattern/PatternHomework.java
@@ -28,16 +28,30 @@ public class PatternHomework {
 //        }
 
         // pattern 3
+//        int n =6;
+//        for(int i=n; i>=1; i--) {
+//            for(int j=1; j<=n-i; j++){
+//                System.out.print(" ");
+//            }
+//            for(int j=1; j<=i; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+        // pattern 4
         int n =6;
-        for(int i=n; i>=1; i--) {
-            for(int j=1; j<=n-i; j++){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<= 2 * (n-i); j++) {
                 System.out.print(" ");
             }
-            for(int j=1; j<=i; j++) {
-                System.out.print("*");
+            for(int j=i; j>=1; j--){
+                System.out.print(j + " ");
+            }
+            for(int j=2; j<=i; j++){
+                System.out.print(j + " ");
             }
             System.out.println();
         }
-
     }
 }
