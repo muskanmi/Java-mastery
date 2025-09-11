dfa05a34f414c5c8c4dbc9f31ad7cb0af8099f3a
Muskan Mittal <mittalmuskan758@gmail.com>
2025-09-11 11:45:21 +0530

    solving array problems and sorting algorithnm

--- src/arrays/SortingAlgorithms.java
+++ src/arrays/SortingAlgorithms.java
@@ -3,8 +3,8 @@ package arrays;
 public class SortingAlgorithms {
     public static void main(String[] args) {
         int arr[] = {9, 5, 3, 1, 2};
-//        bubbleSort(arr);
-//        selectionSort(arr);
+        bubbleSort(arr);
+        selectionSort(arr);
         insertionSort(arr );
         for(int a: arr){
             System.out.print(a+ " ");
