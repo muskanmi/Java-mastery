2e66c201a8d10dbe7dc107d16c62dac87ec302fe
Muskan Mittal <mittalmuskan758@gmail.com>
2025-09-11 19:23:32 +0530

    learning classes and objects

--- src/oops1/MainClass.java
+++ src/oops1/MainClass.java
@@ -3,6 +3,7 @@ package oops1;
 public class MainClass {
     public static void main(String[] args) {
         Dog d1 = new Dog();
+        d1.name = "Tommy";
         d1.bark();
     }
 }
@@ -13,10 +14,10 @@ class Dog {
     String color;
 
     void walk() {
-        System.out.println("Dog is walking");
+        System.out.println(name + " is walking");
     }
 
     void bark() {
-        System.out.println("Dog is barking");
+        System.out.println(name + " is barking");
     }
 }
