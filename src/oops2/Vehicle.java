f3f59717b8d5368bf8bd86767b32c47e6a345fa2
Muskan Mittal <mittalmuskan758@gmail.com>
2025-09-15 08:00:31 +0530

    learning inheritrance in java

--- src/oops2/Vehicle.java
+++ src/oops2/Vehicle.java
@@ -14,6 +14,7 @@ public class Vehicle {
 
     String model;
 
+    // default constructor which always java calls automatically
     Vehicle(){
         System.out.println("Creating a vehicle instance");
     }
