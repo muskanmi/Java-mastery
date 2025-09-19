3557fb69e898f9c3571eee06eac3e3fb3b6805ad
Muskan Mittal <mittalmuskan758@gmail.com>
2025-09-19 11:52:07 +0530

    learning exception handling

--- src/exceptionHandling/MainClass.java
+++ src/exceptionHandling/MainClass.java
@@ -4,7 +4,11 @@ public class MainClass {
     public static void main(String[] args) {
         int a[] = new int[5];
 
-        System.out.println(a[8]);
+        try {
+            System.out.println(a[8]);
+        } catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("Tried to access but gave out of bound");
+        }
 
         System.out.println("Hello guyz"); // not printed (runtime exception)
     }
