21b183630f16ca08f678077b5d2e844139a17eb3
Muskan Mittal <mittalmuskan758@gmail.com>
2025-09-19 12:07:04 +0530

    learning exception handling

---
+++ src/exceptionHandling/FinallyBlockInException.java
@@ -0,0 +1,15 @@
+package exceptionHandling;
+
+public class FinallyBlockInException {
+    public static void main(String[] args) {
+        int a[] = new int[5];
+
+        try{
+            System.out.println(a[8]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Exception handled");
+        } finally {
+            System.out.println("I will run always");
+        }
+    }
+}
