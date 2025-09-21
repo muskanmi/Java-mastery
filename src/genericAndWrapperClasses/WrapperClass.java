bc8c1363b92147a80a5402a9d5c81447c148adef
Muskan Mittal <mittalmuskan758@gmail.com>
2025-09-21 18:27:36 +0530

    leanred wrapper classes in java

---
+++ src/genericAndWrapperClasses/WrapperClass.java
@@ -0,0 +1,14 @@
+package genericAndWrapperClasses;
+
+public class WrapperClass {
+    public static void main(String[] args) {
+
+        Integer obj = new Integer(12);
+
+        Integer obj2 = Integer.valueOf(12);
+
+        Integer obj3 = 12; // autoboxing
+
+        int age = obj;
+    }
+}
