8539f2163874324bbf7bfa47df164cd846a555a3
Muskan Mittal <mittalmuskan758@gmail.com>
2025-09-21 18:43:42 +0530

    leanred generic classes in java

---
+++ src/genericAndWrapperClasses/GenericMethods.java
@@ -0,0 +1,12 @@
+package genericAndWrapperClasses;
+
+public class GenericMethods {
+
+    public static void main(String[] args) {
+        printData("Hello");
+    }
+
+    static void printData(String data) {
+        System.out.println(data);
+    }
+}
