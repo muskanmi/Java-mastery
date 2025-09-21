067dd867ece47ba6c02518e6715865df5b3dfb4b
Muskan Mittal <mittalmuskan758@gmail.com>
2025-09-21 18:48:45 +0530

    leanred generic classes in java

--- src/genericAndWrapperClasses/GenericMethods.java
+++ src/genericAndWrapperClasses/GenericMethods.java
@@ -4,9 +4,16 @@ public class GenericMethods {
 
     public static void main(String[] args) {
         printData("Hello");
+
+        GenericMethods obj = new GenericMethods();
+        obj.doubleData("data2");
     }
 
     static <E> void printData(E data) {
+        System.out.println(data);
+    }
+
+    <E> void doubleData(E data) {
         System.out.println(data);
     }
 }
