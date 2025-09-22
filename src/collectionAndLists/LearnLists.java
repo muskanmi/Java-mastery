202385076ea32a3d150b480b44af3fd6c87cc347
Muskan Mittal <mittalmuskan758@gmail.com>
2025-09-22 17:23:09 +0530

    leanred collections in java

---
+++ src/collectionAndLists/LearnLists.java
@@ -0,0 +1,14 @@
+package collectionAndLists;
+
+import java.util.*;
+
+public class LearnLists {
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20); // dynamic memory
+
+        System.out.println(list);
+    }
+}
