f714d73085c99702c0e4beee1a938cd32ff4f535
Muskan Mittal <mittalmuskan758@gmail.com>
2025-09-24 17:28:23 +0530

    leanred queue in java

--- src/collectionQueueAndSet/LearnQueue.java
+++ src/collectionQueueAndSet/LearnQueue.java
@@ -13,5 +13,10 @@ public class LearnQueue {
         q.offer(40); // this will also add a element
 
         System.out.println(q);
+
+        System.out.println(q.poll()); // first in first out
+        System.out.println(q);
+        System.out.println(q.peek()); // just tell which needs to remove next
+        System.out.println(q);
     }
 }
