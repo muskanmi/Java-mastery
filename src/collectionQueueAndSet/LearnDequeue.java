bb6c4b8465d276bb17961a325fb6aed9392b3bde
Muskan Mittal <mittalmuskan758@gmail.com>
2025-09-24 17:57:37 +0530

    leanred Dequeue in java

--- src/collectionQueueAndSet/LearnDequeue.java
+++ src/collectionQueueAndSet/LearnDequeue.java
@@ -9,12 +9,27 @@ public class LearnDequeue {
 
 //      Queue<Integer> dq = new ArrayDeque<>(); // if i will queue then only queue method will call. dequeue methods will not call.
         Deque<Integer> dq = new ArrayDeque<>();
+        ArrayDeque stack = new ArrayDeque<>();
 
         dq.offer(10);
         dq.offerLast(20);
         dq.offerFirst(30);
 
         System.out.println(dq);
+
+        System.out.println(dq.pollLast());
+        System.out.println(dq);
+
+
+//        we can implement stack using arraydeque
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        System.out.println(stack);
+
+        System.out.println(stack.pop());
 
     }
 }
