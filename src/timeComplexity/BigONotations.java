78e1e28fdcbb1cf4f610fc5f5201cf735a62216e
Muskan Mittal <mittalmuskan758@gmail.com>
2025-10-09 10:37:59 +0530

    learning time complexity in java

--- src/timeComplexity/BigONotations.java
+++ src/timeComplexity/BigONotations.java
@@ -3,8 +3,10 @@ package timeComplexity;
 public class BigONotations {
     public static void main(String[] args) {
         int a[] = {3, 7, 1, 7, 23, 5};
-        int key = 3; // this will return in O(1)
+        int key = 3; // this will return in O(1) best case
 //        int key = 10; O(n) n is array length (worst case if key is not found)
+
+        printAllPairsInArray(a);
     }
 
     // will run in O(1) no matter how much length of array
@@ -12,10 +14,20 @@ public class BigONotations {
         return a[0];
     }
 
+    // in worst time complexity it will run O(n) best case O(1)
     static boolean checkIfNumberIsPresent(int a[], int key) {
         for(int element: a){
             if(element == key) return true;
         }
         return false;
+    }
+
+    // O(n^2)
+    static void printAllPairsInArray(int a[]) {
+        for(int i=0; i<a.length; i++){
+            for(int j=i+1; j<a.length; j++){
+                System.out.println(a[i] + " " + a[j]);
+            }
+        }
     }
 }
