89fdfc01ed42ae87d12bdef149d9866c939d0124
Muskan Mittal <mittalmuskan758@gmail.com>
2025-10-11 19:16:32 +0530

    learning time complexity in java

--- src/timeComplexity/BigONotations.java
+++ src/timeComplexity/BigONotations.java
@@ -22,7 +22,7 @@ public class BigONotations {
         return false;
     }
 
-    // O(n^2)
+    // O(n^2)    (Time Complexity)
     static void printAllPairsInArray(int a[]) {
         for(int i=0; i<a.length; i++){
             for(int j=i+1; j<a.length; j++){
